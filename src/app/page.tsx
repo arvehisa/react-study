16aa0b004c536a52b6b157fe9e58bfebdbea0183
arvehisa <arvehisa@gmail.com>
2024-01-31 21:00:23 +0900

    Add Header component to pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import styles from "../styles/page.module.css";
 import { Documents } from "../components/Documents";
 import { Logo } from "../components/Logo";
 import { Headline } from "../components/Headline";
+import { Header } from "../components/Header";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Header />
       <Headline page="index"/>
       <Logo />
       <Documents />
